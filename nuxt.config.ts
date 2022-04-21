16a44072958702766aa58247ac4e7da81963b13a
Colin Lienard <colin.lienard87@orange.fr>
2022-04-21 17:04:04 +0200

    :art: Use only single quotes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { defineNuxtConfig } from "nuxt3";
+import { defineNuxtConfig } from 'nuxt3';
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  css: ["~/styles/reset.scss"],
+  css: ['~/styles/reset.scss'],
   typescript: {
     strict: true,
   },
