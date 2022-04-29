cec1df460694c4268772782589cae22d6e2f3e44
Colin Lienard <colin.lienard87@orange.fr>
2022-04-29 21:54:49 +0200

    :recycle: Remove main tag

--- App.vue
+++ App.vue
@@ -1,16 +1,10 @@
 <template>
-  <main>
-    <NuxtPage />
-  </main>
+  <NuxtPage />
 </template>
 
 <style lang="scss">
 /* stylelint-disable-next-line selector-id-pattern */
 #__nuxt {
   font-size: 0;
-
-  * {
-    font-size: initial;
-  }
 }
 </style>
