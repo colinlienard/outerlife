d551ae76f5da64fac95ca662eb29e7c221e16179
Colin Lienard <colin.lienard87@orange.fr>
2022-05-01 11:49:12 +0200

    :lipstick: Move global styles in a specific file

--- App.vue
+++ App.vue
@@ -1,10 +1,3 @@
 <template>
   <NuxtPage />
 </template>
-
-<style lang="scss">
-/* stylelint-disable-next-line selector-id-pattern */
-#__nuxt {
-  font-size: 0;
-}
-</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt3';
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  css: ['~/styles/reset.scss'],
+  css: ['~/styles/reset.scss', '~/styles/global.scss'],
   typescript: {
     strict: true,
   },
