d19608dbb1af5f11ee0c41c8489b846b956f90cf
Colin Lienard <colin.lienard87@orange.fr>
2022-05-03 21:40:19 +0200

    :sparkles: Implement the newest terrain tiles

--- Game/Entities/Terrains/TerrainTiles.ts
+++ Game/Entities/Terrains/TerrainTiles.ts
@@ -2,114 +2,259 @@ const TerrainTiles: {
   [key: string]: { source: string; x: number; y: number };
 } = {
   '001': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 0,
     y: 0,
   },
   '002': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 16,
     y: 0,
   },
   '003': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 32,
     y: 0,
   },
+  '0035': {
+    source: 'terrain-001',
+    x: 48,
+    y: 0,
+  },
   '004': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 0,
     y: 16,
   },
   '005': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 16,
     y: 16,
   },
   '006': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 32,
     y: 16,
   },
   '007': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 48,
     y: 16,
   },
   '008': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 0,
     y: 32,
   },
   '009': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 16,
     y: 32,
   },
   '010': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 32,
     y: 32,
   },
   '011': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 48,
     y: 32,
   },
   '012': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 0,
     y: 48,
   },
   '013': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 16,
     y: 48,
   },
+  '014': {
+    source: 'terrain-001',
+    x: 32,
+    y: 48,
+  },
   '015': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 48,
     y: 48,
   },
   '016': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 0,
     y: 64,
   },
   '017': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 16,
     y: 64,
   },
   '018': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 32,
     y: 64,
   },
   '020': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 48,
     y: 64,
   },
   '021': {
-    source: 'planet1',
+    source: 'terrain-001',
     x: 64,
-    y: 16,
+    y: 0,
   },
   '022': {
-    source: 'planet1',
+    source: 'terrain-001',
+    x: 80,
+    y: 0,
+  },
+  '023': {
+    source: 'terrain-001',
+    x: 64,
+    y: 16,
+  },
+  '024': {
+    source: 'terrain-001',
     x: 80,
     y: 16,
   },
-  '023': {
-    source: 'planet1',
-    x: 64,
+  '025': {
+    source: 'terrain-001',
+    x: 96,
+    y: 0,
+  },
+  '026': {
+    source: 'terrain-001',
+    x: 112,
+    y: 0,
+  },
+  '027': {
+    source: 'terrain-001',
+    x: 128,
+    y: 0,
+  },
+  '028': {
+    source: 'terrain-001',
+    x: 144,
+    y: 0,
+  },
+  '0285': {
+    source: 'terrain-001',
+    x: 160,
+    y: 0,
+  },
+  '029': {
+    source: 'terrain-001',
+    x: 96,
+    y: 16,
+  },
+  '030': {
+    source: 'terrain-001',
+    x: 112,
+    y: 16,
+  },
+  '031': {
+    source: 'terrain-001',
+    x: 128,
+    y: 16,
+  },
+  '032': {
+    source: 'terrain-001',
+    x: 144,
+    y: 16,
+  },
+  '0325': {
+    source: 'terrain-001',
+    x: 160,
+    y: 16,
+  },
+  '033': {
+    source: 'terrain-001',
+    x: 96,
     y: 32,
   },
-  '024': {
-    source: 'planet1',
-    x: 80,
+  '034': {
+    source: 'terrain-001',
+    x: 112,
     y: 32,
+  },
+  '035': {
+    source: 'terrain-001',
+    x: 128,
+    y: 32,
+  },
+  '036': {
+    source: 'terrain-001',
+    x: 144,
+    y: 32,
+  },
+  '0365': {
+    source: 'terrain-001',
+    x: 160,
+    y: 32,
+  },
+  '037': {
+    source: 'terrain-001',
+    x: 96,
+    y: 48,
+  },
+  '038': {
+    source: 'terrain-001',
+    x: 112,
+    y: 48,
+  },
+  '039': {
+    source: 'terrain-001',
+    x: 128,
+    y: 48,
+  },
+  '040': {
+    source: 'terrain-001',
+    x: 144,
+    y: 48,
+  },
+  '0405': {
+    source: 'terrain-001',
+    x: 160,
+    y: 48,
+  },
+  '041': {
+    source: 'terrain-001',
+    x: 96,
+    y: 80,
+  },
+  '042': {
+    source: 'terrain-001',
+    x: 96,
+    y: 64,
+  },
+  '043': {
+    source: 'terrain-001',
+    x: 112,
+    y: 64,
+  },
+  '044': {
+    source: 'terrain-001',
+    x: 128,
+    y: 64,
+  },
+  '045': {
+    source: 'terrain-001',
+    x: 144,
+    y: 64,
+  },
+  '046': {
+    source: 'terrain-001',
+    x: 160,
+    y: 64,
+  },
+  '0465': {
+    source: 'terrain-001',
+    x: 160,
+    y: 80,
   },
 };
 
