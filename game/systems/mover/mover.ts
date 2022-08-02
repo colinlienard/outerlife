0d625a6ef9ae9ec0f0b37a50015193fbb858116d
Tom Lienard <tom.lienrd@gmail.com>
2022-08-02 09:27:04 +0000

    feat: type-safe emitter

--- game/systems/mover/mover.ts
+++ game/systems/mover/mover.ts
@@ -1,31 +1,42 @@
 import { Animation, Velocity, Position, Input } from '~~/game/components';
+
 import { Dust } from '~~/game/entities';
+
 import { Emitter, System } from '~~/game/utils';
 
 export class Mover extends System {
   protected readonly requiredComponents = [
     Position,
+
     Velocity,
+
     Animation,
+
     Input,
   ];
 
   update() {
     this.entities.forEach((entity) => {
       const animator = entity.get(Animation);
+
       const position = entity.get(Position);
+
       const velocity = entity.get(Velocity);
+
       const { input } = entity.get(Input);
 
       // If a key is pressed
+
       const moving = Object.values(input).reduce(
         (previous, current) => previous || current
       );
+
       animator.currentAnimation = moving
         ? animator.animations.run
         : animator.animations.idle;
 
       // Handle easing of the speed (acceleration and deceleration)
+
       if (moving) {
         if (velocity.speed < velocity.maxSpeed) {
           velocity.speed += velocity.acceleration;
@@ -39,7 +50,9 @@ export class Mover extends System {
       }
 
       // TODO: only for the player
+
       // Spawn a dust when running
+
       if (
         moving &&
         animator.frameWaiter === 0 &&
@@ -49,7 +62,9 @@ export class Mover extends System {
       }
 
       // Avoid player going too fast when running diagonally
+
       let { speed } = velocity;
+
       if (
         speed > 0 &&
         (input.up || input.down) &&
@@ -59,43 +74,59 @@ export class Mover extends System {
       }
 
       // Handle the direction of the player
+
       if (input.up) {
         velocity.direction.y = 'up';
+
         animator.row = 0;
       } else if (input.down) {
         velocity.direction.y = 'down';
+
         animator.row = 1;
       } else if (moving) {
         velocity.direction.y = null;
       }
+
       if (input.left) {
         velocity.direction.x = 'left';
+
         animator.row = 2;
       } else if (input.right) {
         velocity.direction.x = 'right';
+
         animator.row = 3;
       } else if (moving) {
         velocity.direction.x = null;
       }
 
       // Update the position
+
       switch (velocity.direction.y) {
         case 'up':
           position.y -= speed;
+
           break;
+
         case 'down':
           position.y += speed;
+
           break;
+
         default:
           break;
       }
+
       switch (velocity.direction.x) {
         case 'left':
           position.x -= speed;
+
           break;
+
         case 'right':
           position.x += speed;
+
           break;
+
         default:
           break;
       }
