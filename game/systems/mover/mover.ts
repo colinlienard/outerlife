3133fb007e64b73aed043254dd358168f3763a0e
Colin Lienard <colin.lienard87@orange.fr>
2022-08-26 18:07:48 +0200

    :bug: The patroller sprite was flashing when it was changing animation

--- game/systems/mover/mover.ts
+++ game/systems/mover/mover.ts
@@ -27,6 +27,15 @@ export class Mover extends System {
           (previous, current) => previous || current
         );
 
+        // Set animation to the begining if it changes
+        if (
+          (moving && animation.current === animation.animations.idle) ||
+          (!moving && animation.current === animation.animations.run)
+        ) {
+          animation.column = 0;
+        }
+
+        // Update animation
         animation.current = moving
           ? animation.animations.run
           : animation.animations.idle;
