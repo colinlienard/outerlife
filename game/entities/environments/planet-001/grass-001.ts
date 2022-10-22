bc7efff7daef18fc99d26edf4811710df0b7d8b1
ColinLienard <colin.lienard87@orange.fr>
2022-10-22 20:35:50 +0200

    :white_check_mark: Fix tests

--- game/entities/environments/planet-001/grass-001.ts
+++ game/entities/environments/planet-001/grass-001.ts
@@ -1,14 +1,9 @@
 import { Entity } from '~~/game/utils';
-import {
-  HealthComponent,
-  PositionComponent,
-  SpriteComponent,
-} from '~~/game/components';
+import { PositionComponent, SpriteComponent } from '~~/game/components';
 
 export class Grass001 extends Entity {
   constructor(x: number, y: number) {
     super();
-    this.add(new HealthComponent(1));
     this.add(new PositionComponent(x, y, 16, 16));
     this.add(
       new SpriteComponent('/sprites/environments-001.png', 0, 160, 16, 16)
